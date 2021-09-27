1be659f37228fe2ce3ef4be8a89694c2b160fcfb
AxzelBC <Alejo13580@gmail.com>
2021-09-27 13:04:10 -0500

    Adición de wait y assert.

--- main.c
+++ main.c
@@ -24,7 +24,6 @@ int main(void){
         /**
          * @brief Almacena le comando ingresado.
          */ 
-        char palabra[] = "ls -lAh";
         char comando[128];
         promtShell();
         scanf(" %127[^\n]",comando);
@@ -40,9 +39,10 @@ int main(void){
         }
 
         /**
-         * @brief Creación de subproceso para los comandos
+         * @brief Creación de subproceso para los comandos, y verificación del PID.
          */
         pid_t pid = fork();
+        assert(pid >= 0);
 
         int id = getpid();
 
@@ -52,8 +52,16 @@ int main(void){
             readCommand(comando);
         }
         else if (pid > 0){ // Parent process (dad)
-            wait(NULL);
-            printf("No pase.\n");
+            wait(&status);
+            if (WIFEXITED(status)){ /* Se verifica que el estado es de salida. */
+                if (!(WEXITSTATUS(status)))
+                    printf("\n:)\n");
+                else
+                    printf("\n:(\n");
+            }
+            else{
+                return 1;
+            }
         }
         else{ // Error in PID
             perror("Error en la creación de pid\n");
