f68cc4c7361ef2e1d598996a3e29add2e9d3dfc9
timetravel-1010 <53354136+timetravel-1010@users.noreply.github.com>
2021-09-30 22:04:13 -0500

    Se agrega nombre y código del segundo autor del miniproyecto.

--- main.c
+++ main.c
@@ -2,6 +2,8 @@
  * @file main.c
  * @author Alejandro C. Mosquera (AxzelBC)
  * @code 2022499
+ * @author Daniel F. Vélez C. 
+ * @code 1924306
  * @brief Archivo principal de la shell.
  * @version 0.1
  * @date 2021-04-19
